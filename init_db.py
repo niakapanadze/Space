d22d57717efcd8b84240791f77e1ddaff01f7388
Nia Kapanadze <niakapanadze2012@gmail.com>
2026-06-08 16:09:42 +0400

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,5 +1,5 @@
 from ext import app, db
-from models import Planet, User # <--- Added User here
+from models import Planet, User
 
 with app.app_context():
     db.drop_all()  # წაშალე ყველა ცხრილი რაც კი ბაზაში არის
